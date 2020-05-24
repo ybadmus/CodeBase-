2f2dce72566ca91e17bce16c1e4f5689051ac780
Yusuf Badmus <yusuf.badmus@persol.net>
2020-05-24 10:30:11 +0000

    updates to szLevel

--- ITAPS-HOST/wwwroot/js/applications/user-rights.js
+++ ITAPS-HOST/wwwroot/js/applications/user-rights.js
@@ -3,17 +3,17 @@ $(document).ready(function () {
 });
 
 var switchTilesByUserRight = function (response) {
-    if(response[0].szGroupName.toLowerCase() === "officer") {
+    if (response[0].szLevel.toLowerCase() === "officer") {
         $("#approveTCC").hide();
         $("#processTCC").show();
         $("#assignTCC").hide();
         $("#allTCC").show();
-    } else if (response[0].szGroupName.toLowerCase() === "manager") {
+    } else if (response[0].szLevel.toLowerCase() === "manager") {
         $("#approveTCC").show();
         $("#processTCC").hide();
         $("#assignTCC").hide();
         $("#allTCC").show();
-    } else if (response[0].szGroupName.toLowerCase() === "supervisor") {
+    } else if (response[0].szLevel.toLowerCase() === "supervisor") {
         $("#approveTCC").hide();
         $("#processTCC").hide();
         $("#assignTCC").show();
